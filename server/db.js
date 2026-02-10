1658c33f48450a815a9ccf24aceda4d91ee0cea4
Peter Mackenzie <petrushmackenziethebest@gmail.com>
2026-02-10 10:38:48 -0500

    Use bundled Supabase CA cert path for DB SSL

--- server/db.js
+++ server/db.js
@@ -2,15 +2,13 @@ const { Pool } = require('pg');
 const fs = require('fs');
 const path = require('path');
 
-const isProduction = process.env.NODE_ENV === 'production';
+const supabaseCaPath = path.join(__dirname, 'certs', 'supabase-ca.crt');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: true,
-        ca: isProduction
-            ? fs.readFileSync('/etc/secrets/supabase-ca.crt').toString()
-            : fs.readFileSync(path.join(__dirname, 'certs/supabase-ca.crt')).toString()
+        ca: fs.readFileSync(supabaseCaPath, 'utf8')
     }
 });
 
