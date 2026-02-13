3d6c5004e9352c1a965f48d3812c616b8e9dbafd
Peter Mackenzie <petrushmackenziethebest@gmail.com>
2026-02-13 17:31:18 -0500

    Add Vercel analytics and speed insights to frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 import App from './App.jsx'
 import ScrollToTop from './components/ScrollToTop.jsx'
 import { initSentry } from './sentry.js'
 import './styles/global.css'
 
 initSentry()
+
+if (import.meta.env.PROD) {
+  inject()
+  injectSpeedInsights()
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
